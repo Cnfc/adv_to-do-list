5bcb9f77dad3cd45f20114731e6167bbe5ebf06c
cnfc <stdashkov@mail.ru>
2017-12-29 19:26:42 +0100

    show routes

--- todos_api/index.js
+++ todos_api/index.js
@@ -1,8 +1,9 @@
 
-var express = require('express');
-var app = express();
-var port = 3000;
-var bodyParser = require('body-parser');
+var express = require('express'),
+    app = express(),
+    port = 3000,
+    bodyParser = require('body-parser');
+
 
 var todoRoutes = require('./routes/todos');
 // var mongo
