05d656315580c71b387036eca0f9ab55795c07bc
cnfc <stdashkov@mail.ru>
2017-12-29 19:55:52 +0100

    + delete router

--- todos_api/routes/todos.js
+++ todos_api/routes/todos.js
@@ -47,6 +47,15 @@ router.put('/:todoId', function(req, res){
 });
 
 
+router.delete('/:todoId' , function(req, res){
+  db.Todo.remove({_id: req.params.todoId})
+  .then(function(){
+    res.json({message: 'We deleted it'})
+  })
+  .catch(function(err){
+    res.send(err);
+  })
+});
 
 
 
